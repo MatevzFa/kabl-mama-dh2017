2d5cefe3aa9de62d4a376c6be94692f302088891
Matevž <matevz@STINKPAD.nonstop.local>
2017-05-21 01:16:57 +0200

    app.js GET and POST routes

--- extension/script.js
+++ extension/script.js
@@ -1,11 +1,28 @@
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+const serverURL = 'http://127.0.0.1:8080';
+
+function tabChangeListener(tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete') {
-        chrome.tabs.query({}, (tabs)=> {
+        chrome.tabs.query({}, (tabs) => {
+            tabs = tabs.map((t) => { return t.url });
+            let xhr = new XMLHttpRequest();
+
+            $.ajax({
+                type: 'POST',
+                url: serverURL + '/openedtabs',
+                data: {
+                    tabs: tabs
+                }
+            });
             for (let i = 0; i < tabs.length; i++)
-                console.log('%d: %s', i+1, tabs[i].url);
+                console.log('%d: %s', i + 1, tabs[i]);
         });
     }
-});
+}
 
+chrome.tabs.onUpdated.addListener(tabChangeListener);
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+    console.log(tabId);
+    console.log(removeInfo);
+})
 const a = 500;
---
+++ server/app.js
@@ -0,0 +1,51 @@
+
+if (!process.env.PORT)
+    process.env.PORT = 8080;
+
+
+/**
+ * Init
+ */
+const express = require('express');
+const urlParser = require('url');
+const bodyParser = require('body-parser');
+const badUrls = require('./bad-urls.json').join('|');
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+let existBadTabs = false;
+
+/* GET route for sending true/false based on latest tab POST */
+app.get('/existbadtabs', (req, res) => {
+    if (existBadTabs) {
+        res.send('true');
+    } else {
+        res.send('false');
+    }
+})
+
+/* RegEx for matching bad URLs */
+let urlPattern = '(http|https)\://.*?\.(' + badUrls + ')/';
+
+var regex = new RegExp(urlPattern);
+/* POST route for recieving data about opened tabs */
+app.post('/openedtabs', (req, res) => {
+    let openTabs = req.body.tabs;
+    let badTabs = openTabs.filter((tab) => {
+        return regex.exec(tab) !== null;
+    });
+    existBadTabs = badTabs.length > 0;
+    // console.log(existBadTabs);
+    res.end();
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Server up @ %d', process.env.PORT);
+});
