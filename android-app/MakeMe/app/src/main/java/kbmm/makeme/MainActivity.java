936402e642701e47783cbafb258dc8a375e4b64c
mc0239 <mc0239@student.uni-lj.si>
2017-05-21 09:18:41 +0200

    Update to android-app

--- android-app/MakeMe/app/src/main/java/kbmm/makeme/MainActivity.java
+++ android-app/MakeMe/app/src/main/java/kbmm/makeme/MainActivity.java
@@ -7,13 +7,16 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,19 +29,30 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import java.util.ArrayList;
 import java.util.Objects;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
     private LocationManager locationManager;
+
     private ArrayList<Marker> markers;
     private ArrayList<Circle> circles;
-    private SharedPreferences saves;
+
     private Marker currentPosition;
+
+    private AsyncHttpClient httpClient;
+    private ImageView imageViewChecked;
+
     private boolean checkingPosition;
+    private boolean editMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,28 +60,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         markers = new ArrayList<>();
         circles = new ArrayList<>();
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        httpClient = new AsyncHttpClient();
 
-        Button buttonShrani = (Button) findViewById(R.id.buttonShrani);
-        buttonShrani.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                StringBuilder s = new StringBuilder();
-                for(Marker m : markers) {
-                    s.append(m.getPosition().latitude).append(",").append(m.getPosition().longitude).append("\t");
-                }
-                for(Circle c : circles) c.setVisible(false);
-                for(Marker m : markers) m.setVisible(true);
-                saves.edit().putString("locations", s.toString()).apply();
-            }
-        });
+        imageViewChecked = (ImageView) findViewById(R.id.imageViewChecked);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         Button buttonCheck = (Button) findViewById(R.id.buttonCheck);
         buttonCheck.setOnClickListener(new View.OnClickListener() {
@@ -93,19 +96,71 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     locationManager = (LocationManager) getBaseContext().getSystemService(LOCATION_SERVICE);
                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, MainActivity.this);
                 } catch (SecurityException e) {
-                    Log.d("MY APP", e.getMessage());
+                    Log.e("MakeMe", e.getMessage());
                 }
             }
         });
 
+        final ToggleButton toggleButtonEditLocations = (ToggleButton) findViewById(R.id.toggleButtonEditLocations);
+        toggleButtonEditLocations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!editMode) {
+                    editMode = true;
+                    hideCircles();
+                    showMarkers();
+                } else {
+                    editMode = false;
+                    saveLocations();
+                    showCircles();
+                    hideMarkers();
+                }
+                toggleButtonEditLocations.setChecked(editMode);
+            }
+        });
+
+    }
+
+    private void saveLocations() {
+        Toast.makeText(this, "Locations saved.", Toast.LENGTH_SHORT).show();
+        StringBuilder s = new StringBuilder();
+        for(Marker m : markers) {
+            s.append(m.getPosition().latitude).append(",").append(m.getPosition().longitude).append("\t");
+        }
+        for(Circle c : circles) c.setVisible(false);
+        for(Marker m : markers) m.setVisible(true);
+        getSharedPreferences("mm", Context.MODE_PRIVATE).edit().putString("locations", s.toString()).apply();
+    }
+    private void loadLocations() {
+        String locs = getSharedPreferences("mm", Context.MODE_PRIVATE).getString("locations", "");
+        if(!locs.equals("")) {
+            for(String loc : locs.split("\t")) {
+                String[] ll = loc.split(",");
+                LatLng pos = new LatLng(Double.parseDouble(ll[0]), Double.parseDouble(ll[1]));
+                markers.add(mMap.addMarker(new MarkerOptions().position(pos)));
+                circles.add(mMap.addCircle(new CircleOptions().center(pos).radius(100).strokeColor(R.color.colorPrimaryDark).fillColor(R.color.colorPrimary).visible(false)));
+            }
+        }
+    }
+
+    private void showCircles() {
+        for(Circle c : circles) c.setVisible(true);
+    }
+    private void hideCircles() {
+        for(Circle c : circles) c.setVisible(false);
+    }
+    private void showMarkers() {
+        for(Marker m : markers) m.setVisible(true);
+    }
+    private void hideMarkers() {
+        for(Marker m : markers) m.setVisible(false);
     }
 
     @Override
     protected void onResume() {
 
         LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
-        boolean enabled = service
-                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean enabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
         // check if enabled and if not send user to the GSP settings
         // Better solution would be to display a dialog and suggesting to
@@ -122,28 +177,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        saves = getSharedPreferences("mm", Context.MODE_PRIVATE);
-        String locs = saves.getString("locations", "");
-        if(!locs.equals("")) {
-            for(String loc : locs.split("\t")) {
-                String[] ll = loc.split(",");
-                LatLng pos = new LatLng(Double.parseDouble(ll[0]), Double.parseDouble(ll[1]));
-                markers.add(mMap.addMarker(new MarkerOptions().position(pos)));
-                circles.add(mMap.addCircle(new CircleOptions().center(pos).radius(100).strokeColor(R.color.colorPrimaryDark).fillColor(R.color.colorPrimary)));
-            }
-        }
+        loadLocations();
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(46.0236862,14.6028584), 7.5f));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(46.0236862,14.6028584), 9f));
 
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
-                if(markers.size() < 5) {
+                if(editMode) {
                     markers.add(mMap.addMarker(new MarkerOptions().position(latLng)));
                     circles.add(mMap.addCircle(new CircleOptions().center(latLng).radius(100).strokeColor(R.color.colorPrimaryDark).fillColor(R.color.colorPrimary).visible(false)));
-                } else {
-                    markers.get(4).setPosition(latLng);
-                    circles.get(4).setCenter(latLng);
                 }
             }
         });
@@ -151,8 +194,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
-                markers.remove(marker);
-                marker.remove();
+                if(editMode) {
+                    if(!marker.equals(currentPosition)) {
+                        markers.remove(marker);
+                        for (Circle c : circles) {
+                            if (c.getCenter().equals(marker.getPosition())) {
+                                c.remove();
+                                break;
+                            }
+                        }
+                        marker.remove();
+                    }
+                }
                 return false;
             }
         });
@@ -195,6 +248,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
             if (anyInside) {
                 Toast.makeText(getBaseContext(), "Inside", Toast.LENGTH_LONG).show();
+                //saves.edit().putBoolean("checked", true).apply();
+                imageViewChecked.setImageDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.ic_check_box_black_24dp));
+                httpClient.post(this.getBaseContext(), "https://kabl-mama-dh2071-matevzfa.c9users.io/taskcompleted", null, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        Log.d("HTTP POST", "Success");
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        Log.d("HTTP POST", "Failed");
+                    }
+                });
+
             } else {
                 Toast.makeText(getBaseContext(), "Outside", Toast.LENGTH_LONG).show();
             }
